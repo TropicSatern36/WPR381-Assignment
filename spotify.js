b84b7c8dcc9cf05c36913ee5af2a342899b014c8
TropicSatern36 <hendry.ihlenfeldt@gmail.com>
2024-05-24 17:09:08 +0200

    remove client credentials

--- spotify.js
+++ spotify.js
@@ -2,6 +2,6 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
-  clientId: 'a0055798dcd840dda471c3e37d2eb337',
-  clientSecret: 'bf45e85bf1cb47fd90b9954f6214fd84'
+  clientId: '',
+  clientSecret: ''
 });
