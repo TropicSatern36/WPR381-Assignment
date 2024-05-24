bccd006b10126d53252739987e607403c96ebef6
TropicSatern36 <hendry.ihlenfeldt@gmail.com>
2024-05-24 12:45:05 +0200

    index.html added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Send index.html from public/index.html
+  res.sendFile('index.html', { root: 'public' });
 });
 
 module.exports = router;
